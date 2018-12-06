6db22d2766b0bba213826f4e4d5d94e3f9c04a29
Elzem <elzemmisirli@godre-macbook-pro.local>
2018-12-06 14:58:47 +0300

    Initial Commit

---
+++ FirebaseConnection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseConnection
+//
+//  Created by Elzem on 6.12.2018.
+//  Copyright © 2018 Elzem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
